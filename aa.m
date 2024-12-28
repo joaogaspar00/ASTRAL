5f15ae4f329b981cd8836c596653b3eafbf8be70
João Gaspar <joao.p.gaspar@tecnico.ulisboa.pt>
2024-12-28 03:36:32 +0000

    improved 2d analysis app
    bug corrigidos e integração da app de análise 2d na app principal

--- aa.m
+++ aa.m
@@ -52,7 +52,69 @@ clc
 clear
 close all
 
-[polar, foil] = xfoil('NACA0012', [1 2 3], 1e6, 0,'oper/vpar n 12')
+[polar, foil] = xfoil('NACA0012', 25, 1e6, 0,'oper iter 250 vpar n 12');
+
+CL = polar.CL
+CD = polar.CD
 
 
+figure()
+plot(foil.x, foil.y)
+axis equal
+grid
+xlim([0 1])
 
+%%
+clc
+clear
+close all
+
+% MATLAB Script to Generate Airfoil Coordinates Using XFOIL
+
+% User-defined parameters
+airfoil_name = 'NACA22112'; % Airfoil type (e.g., 'NACA0012', or provide a custom name)
+output_file = './airfoils/coord/NACA0012.dat'; % Output file name
+xfoil_path = '.\sim\aerodynamic_model\XFOIL\xfoil.exe'; % Path to the XFOIL executable
+
+% Create XFOIL input commands
+commands = {
+    sprintf('NACA %s', airfoil_name(5:end)) % Specify the NACA airfoil
+    'PANE'                                  % Refine paneling
+    sprintf('SAVE %s', output_file)         % Save coordinates to file
+    'QUIT'                                  % Exit XFOIL
+};
+
+% Write commands to a temporary file
+input_file = 'xfoil_input.txt';
+fid = fopen(input_file, 'w');
+for i = 1:length(commands)
+    fprintf(fid, '%s\n', commands{i});
+end
+fclose(fid);
+
+% Run XFOIL with the input file
+system(sprintf('%s < %s', xfoil_path, input_file));
+
+% Load and plot the generated airfoil coordinates (optional)
+% Verifique se o arquivo existe
+if isfile(output_file)
+    % Abra o arquivo e leia as coordenadas ignorando a primeira linha
+    fid = fopen(output_file, 'r');
+    fgetl(fid); % Ignora a primeira linha
+    coords = fscanf(fid, '%f %f', [2 Inf])'; % Lê os dados numéricos
+    fclose(fid);
+    
+    % Plote as coordenadas
+    figure;
+    plot(coords(:, 1), coords(:, 2), 'b-');
+    axis equal;
+    grid on;
+    title(sprintf('Airfoil: %s', airfoil_name));
+    xlabel('x-coordinate');
+    ylabel('y-coordinate');
+else
+    error('Failed to generate airfoil coordinates. Check your XFOIL setup.');
+end
+
+% Clean up temporary files
+delete(input_file);
