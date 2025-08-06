815109922cd74864d4736e7e71dea70885b736ec
joaogaspar00 <jpsg.joao.gaspar@gmail.com>
2025-08-06 02:01:08 +0100

    teste

--- setup.m
+++ setup.m
@@ -1,13 +1,12 @@
-clc
-clear
-close all
-
 addpath(genpath('airfoils'));
 addpath(genpath('app'));
-addpath(genpath('exe'));
 addpath(genpath('sim'));
-addpath(genpath('studies'));
 
 appName = './app/ASTRAL.mlapp';
 appDesignerPath = fullfile(pwd, appName); 
 open(appDesignerPath); 
+
+
+clear
+close all
+clc
