65e021b0acaa22e139ecfbd5cdc8bf2b7e9fdab1
João Gaspar <jpsg.joao.gaspar@gmail.com>
2025-10-29 20:00:33 +0100

    Update mathjax-config.js
    turn on \boldsymbol{} option within mathajx

--- docs/js/mathjax-config.js
+++ docs/js/mathjax-config.js
@@ -1,5 +1,11 @@
 window.MathJax = {
+  // Load the boldsymbol extension
+  loader: { load: ['[tex]/boldsymbol'] },
+
   tex: {
+    packages: { '[+]': ['boldsymbol'] },
+
+    // Define macros
     macros: {
       unit: ["\\mathrm{#1}", 1]
     }
